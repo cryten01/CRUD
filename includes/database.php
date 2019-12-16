9ed83398e4f95489e27a6dc0c6e887860fb8d376
cryten <cryten101@gmail.com>
2019-12-16 12:13:15 +0100

    Switched to real estate example. Modified insert

--- includes/database.php
+++ includes/database.php
@@ -3,25 +3,32 @@
 
 function connectToDb()
 {
-    $conn_string = "host=localhost port=5432 dbname=animals user=postgres password=1234";
+    $conn_string = "host=localhost port=5432 dbname=realestate user=postgres password=1234";
     $dbConnection = pg_connect($conn_string) or die('Could not connect: ' . pg_last_error());
 }
 
 function showAll()
 {
-    $query = "SELECT * from animals ORDER BY id";
+    $query = "SELECT * from companies ORDER BY id";
     $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
     while ($value = pg_fetch_object($result)) {
-        echo "<tr> <td>$value->id <td>$value->name";
+        echo "<tr> <td>$value->id <td>$value->name <td>$value->address <td>$value->phone_number <td>$value->turnover";
     }
     pg_free_result($result);
 }
 
 function insert()
 {
+    $input_id = $_POST['insert_id'];
     $input_name = $_POST['insert_name'];
-    $query_insert = "insert into animals (name) values ('$input_name');";
+    $input_address = $_POST['insert_address'];
+    $input_phone = $_POST['insert_phone'];
+    $input_turnover = $_POST['insert_turnover'];
+
+    $query_insert = "insert into companies (name, address, phone_number, turnover)
+    values ('$input_name', '$input_address', '$input_phone', '$input_turnover')";
+
     $result = pg_query($query_insert) or die('Query failed: ' . pg_last_error());
 }
 
--- index.php
+++ index.php
@@ -2,26 +2,32 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title> Animal database </title>
+    <title> Real estate database </title>
 </head>
 
 <body>
 
 <?php include_once 'includes/database.php'; ?>
 
-<h2>Animals</h2>
+<h2>Companies</h2>
 <table>
     <tr>
         <th>ID</th>
         <th>Name</th>
+        <th>Address</th>
+        <th>TelNr</th>
+        <th>Turnover</th>
         <?php showAll() ?>
 </table>
 
 <form action="includes/database.php" method="post">
     <br>
     <br>
-    <b> Insert animal: </b>
-    <input type="text" name="insert_name" placeholder="new animal name">
+    <b> Insert company: </b>
+    <input type="text" name="insert_name" placeholder="company name">
+    <input type="text" name="insert_address" placeholder="address">
+    <input type="text" name="insert_phone" placeholder="phone number">
+    <input type="text" name="insert_turnover" placeholder="turnover">
     <button type="submit" name="insert_submit">Insert</button>
     <br>
     <br>
