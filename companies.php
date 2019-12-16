308c6647820fb679229dead293d0c141979a9c1f
cryten <cryten101@gmail.com>
2019-12-16 13:00:54 +0100

    Moved companies to own page

--- companies.php
+++ companies.php
@@ -9,41 +9,38 @@
 
 <?php include_once 'includes/database.php'; ?>
 
-<h2>Agents</h2>
+<h2>Companies</h2>
 <table>
     <tr>
         <th>ID</th>
-        <th>Forname</th>
-        <th>Surname</th>
+        <th>Name</th>
         <th>Address</th>
         <th>Phone number</th>
         <th>Turnover</th>
-        <?php showAllAgents(); ?>
+        <?php showAllCompanies() ?>
 </table>
 
 <form action="includes/database.php" method="post">
     <br>
     <br>
-    <b> Insert agent: </b>
-    <input type="text" name="insert_forname" placeholder="agent forname">
-    <input type="text" name="insert_surname" placeholder="agent surname">
+    <b> Insert company: </b>
+    <input type="text" name="insert_name" placeholder="company name">
     <input type="text" name="insert_address" placeholder="address">
     <input type="text" name="insert_phone" placeholder="phone number">
     <input type="text" name="insert_turnover" placeholder="turnover">
     <button type="submit" name="insert_submitAgent">Insert</button>
     <br>
     <br>
-    <b> Update agent: </b>
+    <b> Update company: </b>
     <input type="text" name="update_id" placeholder="id">
-    <input type="text" name="update_forname" placeholder="agent forname">
-    <input type="text" name="update_surname" placeholder="agent surname">
+    <input type="text" name="update_name" placeholder="company name">
     <input type="text" name="update_address" placeholder="address">
     <input type="text" name="update_phone" placeholder="phone number">
     <input type="text" name="update_turnover" placeholder="turnover">
     <button type="submit" name="update_submit">Update</button>
     <br>
     <br>
-    <b> Delete agent: </b>
+    <b> Delete company: </b>
     <input type="text" name="delete_id" placeholder="id">
     <button type="submit" name="delete_submit">Delete</button>
 </form>
@@ -51,4 +48,5 @@
 <div id="button"><a href="index.php">Go back to index page</a></div>
 
 </body>
+
 </html>
