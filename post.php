4b58f533417d93da81c1d46720f4ae2cbfad41d9
cryten <cryten101@gmail.com>
2019-12-18 11:14:47 +0100

    Fixed headers already sent by using html href

--- post.php
+++ post.php
@@ -9,19 +9,22 @@ include_once 'queriesCompanies.php';
 if (isset($_POST['insert_submitCompany'])) {
     insertCompany();
     showAllCompanies();
-    header("Location: companies.php?insert=success");
+//    header("Location: companies.php?insert=success");
+    echo("<script>location.href ='companies.php?insert=success'; </script>");
 }
 
 if (isset($_POST['update_submitCompany'])) {
     updateCompany();
     showAllCompanies();
-    header("Location: companies.php?update=success");
+//    header("Location: companies.php?update=success");
+    echo("<script>location.href ='companies.php?update=success'; </script>");
 }
 
 if (isset($_POST['delete_submitCompany'])) {
     delete('companies');
     showAllCompanies();
-    header("Location: companies.php?delete=success");
+//    header("Location: companies.php?delete=success");
+    echo("<script>location.href ='companies.php?delete=success'; </script>");
 }
 
 
@@ -29,19 +32,22 @@ if (isset($_POST['delete_submitCompany'])) {
 if (isset($_POST['insert_submitAgent'])) {
     insertAgent();
     showAllAgents();
-    header("Location: agents.php?insert=success");
+//    header("Location: agents.php?insert=success");
+    echo("<script>location.href ='agents.php?insert=success'; </script>");
 }
 
 if (isset($_POST['update_submitAgent'])) {
     updateAgent();
     showAllAgents();
-    header("Location: agents.php?update=success");
+//    header("Location: agents.php?update=success");
+    echo("<script>location.href ='agents.php?update=success'; </script>");
 }
 
 if (isset($_POST['delete_submitAgent'])) {
     delete('agents');
     showAllAgents();
-    header("Location: agents.php?delete=success");
+//    header("Location: agents.php?delete=success");
+    echo("<script>location.href ='agents.php?delete=success'; </script>");
 }
 
 
@@ -49,19 +55,22 @@ if (isset($_POST['delete_submitAgent'])) {
 if (isset($_POST['insert_submitBuilding'])) {
     insertBuilding();
     showAllBuildings();
-    header("Location: buildings.php?insert=success");
+//    header("Location: buildings.php?insert=success");
+    echo("<script>location.href ='buildings.php?insert=success'; </script>");
 }
 
 if (isset($_POST['update_submitBuilding'])) {
     updateBuilding();
     showAllBuildings();
-    header("Location: buildings.php?update=success");
+//    header("Location: buildings.php?update=success");
+    echo("<script>location.href ='buildings.php?update=success'; </script>");
 }
 
 if (isset($_POST['delete_submitBuilding'])) {
     delete('buildings');
     showAllBuildings();
-    header("Location: buildings.php?delete=success");
+//    header("Location: buildings.php?delete=success");
+    echo("<script>location.href ='buildings.php?delete=success'; </script>");
 }
 
 
